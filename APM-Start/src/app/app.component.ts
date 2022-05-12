f38fc9430b6ff17045f6707edaf024e7ed17fed9
reched <darius.rechesan@cpp.canon>
2022-05-12 14:07:18 +0300

    Services and Dependency Injection

--- APM-Start/src/app/app.component.ts
+++ APM-Start/src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
   selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+      <div> 
+        <h1>{{pageTitle}}</h1>
+        <div> My first component </div>
+        <div>
+          <pm-products></pm-products>
+        </div>
+      </div>
+      `
 })
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  pageTitle: string = 'Acme Product Management'; 
 }
---
+++ APM-Start/src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,9 @@
+import { Pipe, PipeTransform } from "@angular/core";
+@Pipe({
+    name:"convertToSpace"
+})
+export class ConvertToSpacesPipe implements PipeTransform{
+    transform(value: string, character:string){
+    return value.replace(character, ' ');
+}
+}
